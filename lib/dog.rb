79fc402ccf2d77db54001bbb0ecba4f6b59de871
github username <github email address>
2019-07-20 17:17:16 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    attr_accessor :name
+    def name
+        @name
+    end
+    def name=(name)
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+    
+end
